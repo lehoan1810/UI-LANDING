2ba71aad91ea5fa6314339ee4ef2ea74f52dc102
lehoan1810 <hoanhao18102000@gmail.com>
2022-11-17 15:37:29 +0700

    lehoan done

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ const handleToggleMenu = () => {
 	});
 };
 handleToggleMenu();
+
+let navbar = document.querySelector(".header");
+window.onscroll = () => {
+	navbar.classList.remove("active");
+
+	if (window.scrollY > 0) {
+		document.querySelector(".header").classList.add("active");
+	} else {
+		document.querySelector(".header").classList.remove("active");
+	}
+};
+
+window.onload = () => {
+	if (window.scrollY > 0) {
+		document.querySelector(".header").classList.add("active");
+	} else {
+		document.querySelector(".header").classList.remove("active");
+	}
+};
